1f35454b405d5953227766dc60294a0375d5cc47
Nagmat1 <nagmat4@hotmail.com>
2026-02-11 22:33:10 +0100

    Add EventAnnotation test command & handler
    Register a "Test EventAnnotationScanner" command, handler and context-menu entry in plugin.xml and add corresponding keys in plugin.properties. Set Bundle-Localization in MANIFEST.MF. Implement EventAnnotationTestHandler which resolves the selected ICompilationUnit, runs EventAnnotationScanner on it, and logs discovered EventSubscriberInfo results. Also fix trailing newline in plugin.properties.

---
+++ src/org/smartbit4all/eclipse/event/ui/commands/EventAnnotationTestHandler.java
@@ -0,0 +1,78 @@
+package org.smartbit4all.eclipse.event.ui.commands;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.smartbit4all.eclipse.event.ast.EventAnnotationScanner;
+import org.smartbit4all.eclipse.event.core.EventLogger;
+import org.smartbit4all.eclipse.event.core.EventSubscriberInfo;
+
+/**
+ * Runs EventAnnotationScanner on the selected Java compilation unit.
+ */
+public class EventAnnotationTestHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        ICompilationUnit unit = resolveCompilationUnit(event);
+        if (unit == null) {
+            EventLogger.info("EventAnnotationTestHandler: No ICompilationUnit selected");
+            return null;
+        }
+
+        EventAnnotationScanner scanner = new EventAnnotationScanner();
+        List<?> results = scanner.scanForSubscribers(unit);
+
+        EventLogger.info("EventAnnotationTestHandler: " + unit.getElementName()
+                + " subscribers=" + results.size());
+
+        for (Object item : results) {
+            if (item instanceof EventSubscriberInfo) {
+                EventLogger.info("EventAnnotationTestHandler: " + item.toString());
+            } else {
+                EventLogger.info("EventAnnotationTestHandler: Unknown result type: "
+                        + item.getClass().getName());
+            }
+        }
+
+        return null;
+    }
+
+    private ICompilationUnit resolveCompilationUnit(ExecutionEvent event) {
+        ISelection selection = HandlerUtil.getCurrentSelection(event);
+        if (!(selection instanceof IStructuredSelection)) {
+            return null;
+        }
+
+        Object element = ((IStructuredSelection) selection).getFirstElement();
+        if (element instanceof ICompilationUnit) {
+            return (ICompilationUnit) element;
+        }
+
+        if (element instanceof IJavaElement) {
+            IJavaElement javaElement = (IJavaElement) element;
+            if (javaElement.getElementType() == IJavaElement.COMPILATION_UNIT) {
+                return (ICompilationUnit) javaElement;
+            }
+        }
+
+        if (element instanceof IAdaptable) {
+            IAdaptable adaptable = (IAdaptable) element;
+            Object adapted = adaptable.getAdapter(ICompilationUnit.class);
+            if (adapted instanceof ICompilationUnit) {
+                return (ICompilationUnit) adapted;
+            }
+        }
+
+        return null;
+    }
+}
